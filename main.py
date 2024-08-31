0b8779c12c60d1e494092348d8e4fc3f2a9781c0
umesh jyani <umeshjyani000@gmail.com>
2024-08-31 15:23:37 +0530

    asdf

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ possible_last_char = string.ascii_uppercase
 pdf_file_path = "not2.pdf"
 output_file_path = "output.txt"
 
-def log_to_file(message):
-    with open(output_file_path, "a") as file:
-        file.write("\n" +message + "\n")
+# def log_to_file(message):
+#     with open(output_file_path, "a") as file:
+#         file.write("\n" +message + "\n")
 
 start_time = time.time()
 start_message = f"Script started at {time.strftime('%Y-%m-%d %H:%M:%S')}"
-log_to_file(start_message)
+# log_to_file(start_message)
 
 def check_password_on_pdf(pdf_path, password):
     try:
@@ -25,7 +25,7 @@ def check_password_on_pdf(pdf_path, password):
         return False
     except Exception as e:
         error_message = f"An error occurred: {e}"
-        log_to_file(error_message)
+        # log_to_file(error_message)
         return False
 
 for digits_combination in itertools.product(possible_digits, repeat=3):
@@ -34,17 +34,17 @@ for digits_combination in itertools.product(possible_digits, repeat=3):
         if check_password_on_pdf(pdf_file_path, password):
             time_taken = time.time() - start_time
             success_message = f"Password found: {password}. Time taken: {time_taken:.2f} seconds."
-            log_to_file(success_message)
+            # log_to_file(success_message)
             print(success_message)
             break
     else:
         continue
     break
 else:
-    log_to_file("Password not found.")
+    # log_to_file("Password not found.")
     print("Password not found.")
 
 time_taken = time.time() - start_time
 final_message = f"Script completed. Total time taken: {time_taken:.2f} seconds."
-log_to_file(final_message)
+# log_to_file(final_message)
 print(final_message)
